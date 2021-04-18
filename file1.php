2f5a27713513bff8cd8417d04d837f6e87ab76fa
Jeffron <Jeffron>
2021-04-17 20:21:50 -0400

    Bootstrap added with more content

---
+++ file1.php
@@ -0,0 +1,144 @@
+<?php include "includes/html-top.php"; ?>
+
+<body>
+
+<header>
+  <div>
+    <a href="index.php">
+      <img src="assets/logo.jpg" alt="logo">
+    </a>
+  </div>
+  <?php include "includes/nav.php";?>
+</header>
+
+<div class="info">
+  <div>
+    <img src="assets/giza_city.jpg">
+  </div>
+  <div class="para">
+    <h2>Introduction</h2>
+    <p>
+      Giza is the second-largest city in Egypt after Cairo and fourth-largest city in Africa after Lagos, Kinshasa and Cairo. It is the capital of Giza Governate with a total population of 8.8 million as of October 2018. It is located on the west bank of the Nile, 4.9 km southwest of central Cairo, and is a core city of the Greater Cairo metropolis. Giza is most famous as the location of the Giza Plateau, the site of the some of the most imporessive and ancient monuments in the world.
+    </p>
+
+    <p>
+      Giza is a plateau southwest of modern Cairo which served as the necropolis for the royalty of the Old Kingdom of Egypt. Most famous for the pyramids of Khufu(completed in c. 2560 BCE) and the Great Sphinx(built 2500 BCE),recent excavations on the plateau have revealed numerous private tomb complexes and workers' quarters. The original necropolis of Giza was laid out with amazing precision and skill but, once it became associated with the great kings of Egypt and their pyramids, attracted the attention of less prominent officals of lower rank.
+    </p>
+
+    <p>
+      These officials had enough money and prestige to buy their way into burial plots at Giza but had no regard for the symmetry of the original schematics and had their tombs dug wherever they found available space. This resulted ina number of grave complexes throughout Giza which would not have been authorized by the kings who built the famous pyramids which, thorughout history, have drawn vistors from around the world.
+    </p>
+  </div>
+  <div class="more">
+    <a href="http://erosa4.digitalscholar.rochester.edu/assignment01/assignment01/">Learn more about Giza</a>
+  </div>
+</div>
+
+<div class="info">
+  <div>
+    <img src="assets/giza_map.jpg" alt="Giza- Geography">
+  </div>
+  <div class="para">
+    <h2>Geography & Demographics</h2>
+    <p>
+      The city of Giza is the capital of the Giza Governorate, and is located near the northeast border of this governorate. Technically, Giza may not be an incorporated municipal unit, and therefore not a city at all. In a typical Egyptian fashion, there are two districts within the Governorate with the same name: a kism/qasm and associated markiz. Some 9 urban kisms of Giza Governorate form collectively a contiguous area of 98.4 km2 on the Nile directly opposite Cairo, and recorded a preliminary count of 4,146,340 in 2017 census count, not including the Al-Ḥawāmidiyah kism separated by Giza markiz.
+    </p>
+
+    <p>
+      The city's population was reported as 2,681,863 in the 2006 national census, while the governorate had 6,272,571 at the same census, without specifying what the city is.
+    </p>
+  </div>
+  <div class="more">
+    <a href="http://erosa4.digitalscholar.rochester.edu/assignment01/assignment01/">Learn more about Giza</a>
+  </div>
+</div>
+
+<div class="info">
+  <div>
+    <img src="assets/giza_climate.jpg" alt="Giza - Environment">
+  </div>
+  <div class="para">
+    <h2>Climate</h2>
+    <p>
+        Giza experiences a hot desert climate like arid climate (Köppen: BWh). Its climate is similar to Cairo, owing to its proximity. Wind storms can be frequent across Egypt in spring, bringing Saharan dust into the city during the months of March and April. High temperatures in winter range from 16 to 20 °C (61 to 68 °F), while nighttime lows drop to below 7 °C (45 °F).
+      </p>
+
+      <p>
+        In summer, the highs are 40 °C (104 °F), and the lows can drop to about 20 °C (68 °F). Rain is infrequent in Giza; snow and freezing temperatures are extremely rare. Up to August 2013, the highest recorded temperature was 46 °C (115 °F) on 13 June 1965, while the lowest recorded temperature was 2 °C (36 °F) on 8 January 1966.
+      </p>    
+  </div>
+  <div class="more">
+    <a href="http://erosa4.digitalscholar.rochester.edu/assignment01/assignment01/">Learn more about Giza</a>
+  </div>
+</div>
+
+<div class="info">
+  <div>
+    <img src="assets/giza_history.jpg" alt="Giza - History">
+  </div>
+  <div class="para">
+    <h2>History</h2>
+    <p>
+        The area in what is now Giza served as the necropolis of several pharaohs who ruled ancient Egypt, during the 2nd millennium BC. Three of these tombs, in the form of giant pyramids, are what is now the famed Giza pyramid complex.
+      </p>
+      <p>
+        As ancient Egypt passed under several conquests under the Persians, Greeks, Romans and Byzantines, so did the area in what is now Giza. A Byzantine village named Phylake (Greek: Φυλακή) or Terso (Coptic: ϯⲣⲥⲱ, meaning "the fortress", now Tersa) was located south of Giza and should not be confused with it.
+      </p>
+  </div>
+  <div class="more">
+    <a href="http://erosa4.digitalscholar.rochester.edu/assignment01/assignment01/">Learn more about Giza</a>
+  </div>
+</div>
+
+<div class="info">
+  <div>
+    <img src="assets/giza_pyramids.jpg" alt="Giza - Attraction">
+  </div>
+  <div class="para">
+    <h2>Attractions</h2>
+    <p>
+        Although the Giza plateau is most closely associated with the pyramids of Khufu, Khafre, and Menkaure, the site was used as early as the First Dynasty of Egypt as evidenced by the tomb of the king Djet which was found toward the edge of the plateau. Evidence of at least one king from the Second Dynasty (Nynetjer) buried at Giza has also been found. Further, inscriptions relate how king Khufu had to clear many earlier tombs and grave complexes to build the Great Pyramid.
+    </p>
+
+    <p>
+        What happened to the corpses or the grave goods from those tombs is not known. The Great Pyramid of Khufu (also known as the pyramid of Cheops, the king's Greek name) is the last remaining of the ancient Seven Wonders of the World and rises to a height of 481 feet (147 metres). The pyramid of Khafre is 471 feet tall(144 metres) and that of Menkaure rises to 213 feet (65 metres). The Great Sphinx sits on the eastern side of the plateau apart from the pyramids but it is thought it once was an important part of the pyramid complex which covered the area.
+    </p>
+  </div>
+  <div class="more">
+    <a href="http://erosa4.digitalscholar.rochester.edu/assignment01/assignment01/">Learn more about Giza</a>
+  </div>
+</div>
+
+<div class="info">
+  <div>
+    <img src="assets/giza_baladi_bread.jpg" alt="Giza - Cuisine">
+  </div>
+  <div class="para">
+    <h2>Cuisine</h2>
+    <p>
+      Baladi Bread is one of the most popular foods in Egypt. It is normally served with every meal as a side or to dip into something like a soup, baba ganoush or stew. You are likely to be served Egyptian flatbread at breakfast time with soft cheese, eggs and tahini. You’ll also see it at lunch and dinner in Egypt. It’s actually one of our favourite breads in the world. And the hot fresh breads from the bakery above were a real treat.
+    </p>
+  </div>
+  <div class="more">
+    <a href="http://erosa4.digitalscholar.rochester.edu/assignment01/assignment01/">Learn more about Giza</a>
+  </div>
+</div>
+
+<!-- Footer -->
+<footer>
+  <h2>Sources</h2>
+  <ul class="flowing-boxes">
+    <li><a href="http://bmart13.digitalscholar.rochester.edu/assignment03/index.html" target="_blank">DMS 290 - Assignment 03 - Group San Francisco</a></li>
+    <li><a href="https://en.wikipedia.org/wiki/Giza" target="_blank">Wikipedia Giza</a></li>
+    <li><a href="https://en.wikipedia.org/wiki/Giza#Climate" target="_blank">Giza Climate</a></li>
+    <li><a href="https://www.euronews.com/travel/2021/01/07/egypt-s-giza-pyramids-are-getting-a-revamp-to-boost-tourism" target="_blank">Pyramids in Giza</a></li>
+    <li><a href="https://worldtravelfamily.com/food-in-egypt/" target="_blank">Food in Egypt Guide</a></li>
+    <li><a href="https://en.wikipedia.org/wiki/Son_et_lumière_(show)" target="_blank">Son et lumière</a></li>
+  </ul>
+  
+</footer>
+
+<?php include "includes/html-bottom.php"; ?>
+
+</body>
+</html>
---
+++ file3.php
@@ -0,0 +1,144 @@
+<?php include "includes/html-top.php"; ?>
+
+<body>
+
+<header>
+    <div>
+      <a href="index.php">
+        <img src="assets/logo.jpg" alt="logo">
+      </a>
+    </div>
+    <?php include "includes/nav.php";?>
+  </header>
+
+<div class="info">
+  <div>
+    <img src="assets/yangzhou_skyline.jpeg" alt="Yangzhou - Skyline">
+  </div>
+  <div class="para">
+    <h2>Introduction</h2>
+    <p>
+      Yangzhou is a prefecture-level city in central Jiangsu Province, China. Historically, Yangzhou was one of the wealthiest cities in China, known at various periods for its great merchant families, poets, artists, and scholars. Its name refers to its former position as the capital of the ancient Yangzhou prefecture in imperial China. Yangzhou was one of the first cities to benefit from one of the earliest world bank loans in China, used to construct Yangzhou thermal power station in 1994.
+    </p>
+
+    <p>
+      Sitting on the north bank of the Yangtze, it borders the provincial capital Nanjing to the southwest, Huai'an to the north, Yancheng to the northeast, Taizhou to the east, and Zhenjiang across the river to the south. Its population was 4,414,681 at the 2010 census and its urban area is home to 2,146,980 inhabitants, including three urban districts, currently in the agglomeration.
+    </p>
+  </div>
+  <div class="more">
+        <!---Link to original city website: Yangzhou--->
+    <a href="http://hli66.digitalscholar.rochester.edu/assignment01/index.html">Learn more about Yangzhou</a>
+  </div>
+</div>
+
+<div class="info">
+  <div>
+    <img src="assets/yangzhou_map.png" alt="Yangzhou - Geography">
+  </div>
+  <div class="para">
+    <h2> Geography & Demographics</h2>
+     <p>
+      Yangzhou is located on a plain north of the Yangtze. The Grand Canal, also known as the Jing-Hang Canal, crosses the prefecture-level from the north to the south; its modern route passes through the eastern outskirts of Yangzhou's main urban area, while its old route runs through the city center. Like much of the entire prefecture-level city, Yangzhou's main urban area (the "city proper") is criss-crossed by an intricate network of canals and small lakes. Other major bodies of water within the prefecture-level city include the Baoshe River, Datong River, Beichengzi River, Tongyang Canal, Xintongyang Canal, Baima Lake, Baoying Lake, Gaoyou Lake and Shaobo Lake.
+    </p>
+
+    <p>
+      Yangzhou's 2021 population is now estimated at 2,039,472. It has grown by 46,699 since 2015, which represents a 2.34% annual change.
+    </p>
+  </div>
+  <div class="more">
+        <!---Link to original city website: Yangzhou--->
+    <a href="http://hli66.digitalscholar.rochester.edu/assignment01/index.html">Learn more about Yangzhou</a>
+  </div>
+</div>
+
+<div class="info">
+  <div>
+    <img src="assets/yangzhou_environment.jpeg" alt="Yangzhou - Environment">
+  </div>
+  <div class="para">
+    <h2>Climate</h2>
+    <p>
+        Yangzhou has a subtropical monsoon climate with humid changeable wind; longer winters for about 4 months, summers 3 months and shorter springs and autumns, 2 months respectively; frost-free period of 222 days and annual average sunshine of 2177 hours. The mean annual temperature is 15.72 °C (60.3 °F) annually; the normal monthly mean 24-hour temperature ranges from 2.5 °C (36.5 °F) in January to 28.0 °C (82.4 °F) in July.
+      </p>
+
+      <p>
+        The annual average precipitation is 1,043 mm (41.1 in), and about 45 percent of rainfall is concentrated in the summer. The rainy season known as "plum rain season" usually lasts from mid-June to late July. During this season, the plums are ripening, hence the name plum rain.
+      </p>
+  </div>
+  <div class="more">
+        <!---Link to original city website: Yangzhou--->
+    <a href="http://hli66.digitalscholar.rochester.edu/assignment01/index.html">Learn more about Yangzhou</a>
+  </div>
+</div>
+
+<div class="info">
+  <div>
+    <img src="assets/yangzhou_history.jpeg" alt="Yangzhou - History">
+  </div>
+  <div class="para">
+    <h2>History</h2>
+    <p>
+        Guangling, the first settlement in the Yangzhou area, was founded in the Spring and Autumn period. After the defeat of Yue by King Fuchai of Wu, a garrison city was built 12 m above the water level on the north bank of the Yangtze c. 485 BC. This city in the shape of a three by three li square was named Hancheng.
+      </p>
+      <p>
+        The newly built Han canal formed a moat around the south and east sides of the city. The purpose of Hancheng was to protect Suzhou from naval invasion from Qi. During the Han dynasty, Guangling was the seat of Guangling Commandery. It constituted a part of the Xu Province, rather than the Yang Province, which was covered the entire southeastern part of China then. In 590, the city was made the capital of a newly established Yang Prefecture, and began to be referred to with its current name.
+      </p>
+  </div>
+  <div class="more">
+        <!---Link to original city website: Yangzhou--->
+    <a href="http://hli66.digitalscholar.rochester.edu/assignment01/index.html">Learn more about Yangzhou</a>
+  </div>
+</div>
+
+<div class="info">
+  <div>
+    <img src="assets/yangzhou_daming_temple.jpeg" alt="Yangzhou - Attraction">
+  </div>
+  <div class="para">
+    <h2>Attractions</h2>
+    <p>
+      Located on Shugang Hill, in the city's northwest, is Fajing Temple, formerly known as DaMing Temple. The original temple was built in Liu Song Dynasty (420–479). A nine-story pagoda, the Qilingta, was built on the temple grounds in the year of Sui Dynasty (589–618) . A recent addition to the temple complex is the Jianzhen Memorial Hall, built according to Tang Dynasty methods and financed with contributions raised by Buddhist groups in Japan. When Qing Emperor Qian Long visited Yangzhou in 1765, he was troubled by The temple's name DaMing (which literally means "Great Ming') fearing that it might revive nostalgia for the Ming Dynasty, which was overthrown by his Manchu predecessors. He had it renamed Fajing Temple. The temple was seriously damaged during the Taiping Rebellion at the beginning of the 20th century. The present structure is a reconstruction dating from the 1930s.
+    </p>
+
+    <p>
+      There are plenty of great spots to capture a snap of the majestic bridge. But if you want a truly postcard-worthy shot, head to the Golden Gate Bridge Vista Point, situated high on a hill overlooking San Francisco. If you have extra time, make sure to explore the Golden Gate National Recreation Area. The actual span of the Golden Gate National Recreation Area encompasses multiple places in San Mateo (south of San Francisco), San Francisco and Marin Counties (Alcatraz and Muir Woods included), but notable parts of this recreation area can be found just a stroll away from the Golden Gate. From the bridge, travelers will find some scenic, bayside trails, some of which lead to secluded beaches, including Kirby Cove and Black Sands Beach. If you really want a trek, journey to the Point Bonita Lighthouse for sweeping views of the bay, found at the very tip of the Golden Gate Strait.
+    </p>
+  </div>
+  <div class="more">
+        <!---Link to original city website: Yangzhou--->
+    <a href="http://hli66.digitalscholar.rochester.edu/assignment01/index.html">Learn more about Yangzhou</a>
+  </div>
+</div>
+
+<div class="info">
+  <div>
+    <img src="assets/yangzhou_fried_rice.jpg" alt="Yangzhou- Cuisine">
+  </div>
+  <div class="para">
+    <h2>Cuisine</h2>
+    <p>
+      Yangzhou fried rice is a perhaps the most well-known dish of the city of Yangzhou, Jiangsu province. The recipe was invented by Qing China's Yi Bingshou (1754–1815) and the dish was named Yangzhou fried rice since Yi was once the regional magistrate of Yangzhou. The difference between Yang Chow Fried Rice and ordinary fried rice is that Yang Chow Style invariably includes a combination of proteins. Rather than using a single protein like shrimp or pork or chicken as the dominant ingredient in fried rice, Yang Chow uses a variety. Most commonly used is a combination of pork, shrimp and frequently chicken or duck. The rest of the ingredients vary as well, but what makes Yang Chow fried rice Yang Chow Style, is the mixture.
+    </p>
+  </div>
+    <div class="more">
+        <!---Link to original city website: Yangzhou--->
+    <a href="http://hli66.digitalscholar.rochester.edu/assignment01/index.html">Learn more about Yangzhou</a>
+  </div>
+</div>
+   
+<footer>
+
+  <h2>Sources</h2>
+  <ul class="flowing-boxes">
+    <li><a href="http://bmart13.digitalscholar.rochester.edu/assignment03/index.html" target="_blank">DMS 290 - Assignment 03 - Group San Francisco</a></li>
+    <li><a href="https://en.wikipedia.org/wiki/Yangzhou" target="_blank">Wikipedia Yangzhou</a></li>
+    <li><a href="https://worldpopulationreview.com/world-cities/yangzhou-population" target="_blank">Yangzhou Population</a></li>
+    <li><a href="https://en.wikipedia.org/wiki/Yangzhou_fried_rice" target="_blank">Yangzhou Cuisine</a></li>
+    <li><a href="https://en.unesco.org/creative-cities/yangzhou" target="_blank">UNESCO Yangzhou</a></li>
+  </ul>
+</footer>
+
+<?php include "includes/html-bottom.php"; ?>
+
+</body>
+</html>
